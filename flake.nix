daaedd4540d5db294bb91db81fcabc0aab44657b
weriomat <engel@weriomat.com>
2026-05-28 21:36:59 +0200

    chore: format flake

--- flake.nix
+++ flake.nix
@@ -15,139 +15,161 @@
     };
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs:
-  {
-    overlays.default = final: prev:
+  outputs =
     {
-      deploy-rs = {
+      self,
+      nixpkgs,
+      utils,
+      ...
+    }@inputs:
+    {
+      overlays.default = final: prev: {
+        deploy-rs = {
 
-        deploy-rs = final.rustPlatform.buildRustPackage {
-          pname = "deploy-rs";
-          version = "0.1.0";
+          deploy-rs = final.rustPlatform.buildRustPackage {
+            pname = "deploy-rs";
+            version = "0.1.0";
 
-          src = final.lib.sourceByRegex ./. [
-            "Cargo\.lock"
-            "Cargo\.toml"
-            "src"
-            "src/bin"
-            ".*\.rs$"
-          ];
+            src = final.lib.sourceByRegex ./. [
+              "Cargo\.lock"
+              "Cargo\.toml"
+              "src"
+              "src/bin"
+              ".*\.rs$"
+            ];
 
-          runtimeInputs = [ final.pkgs.sops ];
+            runtimeInputs = [ final.pkgs.sops ];
 
-          cargoLock.lockFile = ./Cargo.lock;
+            cargoLock.lockFile = ./Cargo.lock;
 
-          meta = {
-            description = "A Simple multi-profile Nix-flake deploy tool"; 
-            mainProgram = "deploy";
+            meta = {
+              description = "A Simple multi-profile Nix-flake deploy tool";
+              mainProgram = "deploy";
+            };
           };
-        };
 
-        lib = rec {
+          lib = rec {
 
-          setActivate = builtins.trace
-            "deploy-rs#lib.setActivate is deprecated, use activate.noop, activate.nixos or activate.custom instead"
-            activate.custom;
+            setActivate = builtins.trace "deploy-rs#lib.setActivate is deprecated, use activate.noop, activate.nixos or activate.custom instead" activate.custom;
 
-          activate = rec {
-            custom =
-              {
-                __functor = customSelf: base: activate:
+            activate = rec {
+              custom = {
+                __functor =
+                  customSelf: base: activate:
                   final.buildEnv {
                     name = ("activatable-" + base.name);
-                    paths =
-                      [
-                        base
-                        (final.writeTextFile {
-                          name = base.name + "-activate-path";
-                          text = ''
-                            #!${final.runtimeShell}
-                            set -euo pipefail
+                    paths = [
+                      base
+                      (final.writeTextFile {
+                        name = base.name + "-activate-path";
+                        text = ''
+                          #!${final.runtimeShell}
+                          set -euo pipefail
 
-                            if [[ "''${DRY_ACTIVATE:-}" == "1" ]]
-                            then
-                                ${customSelf.dryActivate or "echo ${final.writeScript "activate" activate}"}
-                            elif [[ "''${BOOT:-}" == "1" ]]
-                            then
-                                ${customSelf.boot or "echo ${final.writeScript "activate" activate}"}
-                            else
-                                ${activate}
-                            fi
-                          '';
-                          executable = true;
-                          destination = "/deploy-rs-activate";
-                        })
-                        (final.writeTextFile {
-                            name = base.name + "-activate-rs";
-                            text = ''
-                            #!${final.runtimeShell}
-                            exec ${final.deploy-rs.deploy-rs}/bin/activate "$@"
-                          '';
-                          executable = true;
-                          destination = "/activate-rs";
-                        })
-                      ];
+                          if [[ "''${DRY_ACTIVATE:-}" == "1" ]]
+                          then
+                              ${customSelf.dryActivate or "echo ${final.writeScript "activate" activate}"}
+                          elif [[ "''${BOOT:-}" == "1" ]]
+                          then
+                              ${customSelf.boot or "echo ${final.writeScript "activate" activate}"}
+                          else
+                              ${activate}
+                          fi
+                        '';
+                        executable = true;
+                        destination = "/deploy-rs-activate";
+                      })
+                      (final.writeTextFile {
+                        name = base.name + "-activate-rs";
+                        text = ''
+                          #!${final.runtimeShell}
+                          exec ${final.deploy-rs.deploy-rs}/bin/activate "$@"
+                        '';
+                        executable = true;
+                        destination = "/activate-rs";
+                      })
+                    ];
                   };
               };
 
-            nixos = base:
-              (custom // {
-                dryActivate = "$PROFILE/bin/switch-to-configuration dry-activate";
-                boot = "$PROFILE/bin/switch-to-configuration boot";
-              })
-              base.config.system.build.toplevel
-              ''
-                # work around https://github.com/NixOS/nixpkgs/issues/73404
-                cd /tmp
+              nixos =
+                base:
+                (
+                  custom
+                  // {
+                    dryActivate = "$PROFILE/bin/switch-to-configuration dry-activate";
+                    boot = "$PROFILE/bin/switch-to-configuration boot";
+                  }
+                )
+                  base.config.system.build.toplevel
+                  ''
+                    # work around https://github.com/NixOS/nixpkgs/issues/73404
+                    cd /tmp
 
-                $PROFILE/bin/switch-to-configuration switch
+                    $PROFILE/bin/switch-to-configuration switch
 
-                # https://github.com/serokell/deploy-rs/issues/31
-                ${with base.config.boot.loader;
-                final.lib.optionalString systemd-boot.enable
-                "sed -i '/^default /d' ${efi.efiSysMountPoint}/loader/loader.conf"}
-              '';
+                    # https://github.com/serokell/deploy-rs/issues/31
+                    ${
+                      with base.config.boot.loader;
+                      final.lib.optionalString systemd-boot.enable "sed -i '/^default /d' ${efi.efiSysMountPoint}/loader/loader.conf"
+                    }
+                  '';
 
-            home-manager = base: custom base.activationPackage "$PROFILE/activate";
+              home-manager = base: custom base.activationPackage "$PROFILE/activate";
 
-            # Activation script for 'darwinSystem' from nix-darwin.
-            # 'HOME=/var/root' is needed because 'sudo' on darwin doesn't change 'HOME' directory,
-            # while 'darwin-rebuild' (which is invoked under the hood) performs some nix-channel
-            # checks that rely on 'HOME'. As a result, if 'sshUser' is different from root,
-            # deployment may fail without explicit 'HOME' redefinition.
-            darwin = base: custom base.config.system.build.toplevel "HOME=/var/root $PROFILE/activate";
+              # Activation script for 'darwinSystem' from nix-darwin.
+              # 'HOME=/var/root' is needed because 'sudo' on darwin doesn't change 'HOME' directory,
+              # while 'darwin-rebuild' (which is invoked under the hood) performs some nix-channel
+              # checks that rely on 'HOME'. As a result, if 'sshUser' is different from root,
+              # deployment may fail without explicit 'HOME' redefinition.
+              darwin = base: custom base.config.system.build.toplevel "HOME=/var/root $PROFILE/activate";
 
-            noop = base: custom base ":";
-          };
+              noop = base: custom base ":";
+            };
 
-          deployChecks = deploy: builtins.mapAttrs (_: check: check deploy) {
-            deploy-schema = deploy: final.runCommand "jsonschema-deploy-system" { } ''
-              ${final.check-jsonschema}/bin/check-jsonschema --schemafile ${./interface.json} ${final.writeText "deploy.json" (builtins.toJSON deploy)} && touch $out
-            '';
+            deployChecks =
+              deploy:
+              builtins.mapAttrs (_: check: check deploy) {
+                deploy-schema =
+                  deploy:
+                  final.runCommand "jsonschema-deploy-system" { } ''
+                    ${final.check-jsonschema}/bin/check-jsonschema --schemafile ${./interface.json} ${final.writeText "deploy.json" (builtins.toJSON deploy)} && touch $out
+                  '';
 
-            deploy-activate = deploy:
-              let
-                profiles = builtins.concatLists (final.lib.mapAttrsToList (nodeName: node: final.lib.mapAttrsToList (profileName: profile: [ (toString profile.path) nodeName profileName ]) node.profiles) deploy.nodes);
-              in
-              final.runCommand "deploy-rs-check-activate" { } ''
-                for x in ${builtins.concatStringsSep " " (map (p: builtins.concatStringsSep ":" p) profiles)}; do
-                  profile_path=$(echo $x | cut -f1 -d:)
-                  node_name=$(echo $x | cut -f2 -d:)
-                  profile_name=$(echo $x | cut -f3 -d:)
+                deploy-activate =
+                  deploy:
+                  let
+                    profiles = builtins.concatLists (
+                      final.lib.mapAttrsToList (
+                        nodeName: node:
+                        final.lib.mapAttrsToList (profileName: profile: [
+                          (toString profile.path)
+                          nodeName
+                          profileName
+                        ]) node.profiles
+                      ) deploy.nodes
+                    );
+                  in
+                  final.runCommand "deploy-rs-check-activate" { } ''
+                    for x in ${builtins.concatStringsSep " " (map (p: builtins.concatStringsSep ":" p) profiles)}; do
+                      profile_path=$(echo $x | cut -f1 -d:)
+                      node_name=$(echo $x | cut -f2 -d:)
+                      profile_name=$(echo $x | cut -f3 -d:)
 
-                  test -f "$profile_path/deploy-rs-activate" || (echo "#$node_name.$profile_name is missing the deploy-rs-activate activation script" && exit 1);
+                      test -f "$profile_path/deploy-rs-activate" || (echo "#$node_name.$profile_name is missing the deploy-rs-activate activation script" && exit 1);
 
-                  test -f "$profile_path/activate-rs" || (echo "#$node_name.$profile_name is missing the activate-rs activation script" && exit 1);
-                done
+                      test -f "$profile_path/activate-rs" || (echo "#$node_name.$profile_name is missing the activate-rs activation script" && exit 1);
+                    done
 
-                touch $out
-              '';
+                    touch $out
+                  '';
+              };
           };
         };
       };
-    };
-  } //
-    utils.lib.eachSystem (utils.lib.defaultSystems ++ ["aarch64-darwin"]) (system:
+    }
+    // utils.lib.eachSystem (utils.lib.defaultSystems ++ [ "aarch64-darwin" ]) (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -186,13 +208,19 @@
         };
 
         checks = {
-          deploy-rs = self.packages.${system}.default.overrideAttrs (super: { doCheck = true; });
-        } // (pkgs.lib.optionalAttrs (pkgs.lib.elem system ["x86_64-linux"]) (import ./nix/tests {
-          inherit inputs pkgs;
-        }));
+          deploy-rs = self.packages.${system}.default.overrideAttrs (super: {
+            doCheck = true;
+          });
+        }
+        // (pkgs.lib.optionalAttrs (pkgs.lib.elem system [ "x86_64-linux" ]) (
+          import ./nix/tests {
+            inherit inputs pkgs;
+          }
+        ));
 
         inherit (pkgs.deploy-rs) lib;
 
         check-matrix = mkMatrix "check" self.checks.${system};
-      });
+      }
+    );
 }
